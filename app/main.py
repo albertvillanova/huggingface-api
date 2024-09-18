2931bdea8a17dd572d072aedfadca1afb05ba0da
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2024-09-18 11:30:17 +0200

    Add user-access endpoints

--- app/main.py
+++ app/main.py
@@ -366,6 +366,58 @@ async def check_repo_auth(
     }
 
 
+@app.get("/{repo_type}s/{repo_id}/user-access-request/{status}")
+async def list_user_access_requests(
+    repo_id: str,
+    status: str,
+    *,
+    repo_type: Optional[str] = None,
+    token: Union[str, bool, None] = None,
+):
+    return {
+        "repo_id": repo_id,
+        "status": status,
+        "repo_type": repo_type,
+        "token": token,
+    }
+
+
+@app.post("/{repo_type}s/{repo_id}/user-access-request/handle")
+async def handle_user_access_request(
+    repo_id: str,
+    user_id: str,
+    *,
+    repo_type: Optional[str] = None,
+    token: Union[str, bool, None] = None,
+    status: Optional[str] = None,
+    message: Optional[str] = None,
+):
+    return {
+        "repo_id": repo_id,
+        "user_id": user_id,
+        "repo_type": repo_type,
+        "token": token,
+        "status": status,
+        "message": message,
+    }
+
+
+@app.post("/models/{repo_id}/user-access-request/grant")
+async def grant_user_access(
+    repo_id: str,
+    user_id: str,
+    *,
+    repo_type: Optional[str] = None,
+    token: Union[str, bool, None] = None,
+):
+    return {
+        "repo_id": repo_id,
+        "user_id": user_id,
+        "repo_type": repo_type,
+        "token": token,
+    }
+
+
 app.include_router(users.router)
 app.include_router(organizations.router)
 app.include_router(repos.router)
